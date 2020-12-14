bcfd0a1ec31c0f2d158fb7831da9e7507b6da41a
Kiril Simeonov <k.s.simeonov@gmail.com>
2020-12-14 15:46:58 +0200

    Add first interfaces, and Service layer

---
+++ FlexiForum.Data/Interfaces/IForum.cs
@@ -0,0 +1,26 @@
+﻿using FlexiForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexiForum.Data.Interfaces
+{
+    public interface IForum
+    {
+        IEnumerable<Forum> TakeAll();
+
+        IEnumerable<ApplicationUser> TakeAllActiveUsers();
+
+        Forum TakeById(int id);
+
+
+        Task Delete(int forumId);
+
+        Task Create(Forum forum);
+
+        Task UdpateForumTItle(int forumId, string newTitle);
+
+        Task UdpateForumInfo(int forumId, string newInfo);
+    }
+}
---
+++ FlexiForum.Services/ForumService.cs
@@ -0,0 +1,58 @@
+﻿using FlexiForum.Data;
+using FlexiForum.Data.Interfaces;
+using FlexiForum.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FlexiForum.Services
+{
+    public class ForumService : IForum
+    {
+
+        private readonly ApplicationDbContext _context;
+
+
+        public ForumService(ApplicationDbContext context)
+        {
+
+            _context = context;
+        }
+
+        public IEnumerable<Forum> TakeAll()
+        {
+            return _context.Forums.Include(x => x.Posts); //инклудвам navigation property-то
+        }
+        public IEnumerable<ApplicationUser> TakeAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum TakeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UdpateForumInfo(int forumId, string newInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UdpateForumTItle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
